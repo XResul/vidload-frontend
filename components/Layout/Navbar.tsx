5fcf7ca38b7675a3ac3f1f6f29979f279322946c
XResul <resulbinici222@gmail.com>
2025-10-23 11:42:37 +0300

    n

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-import SearchBar from "../Search/SearchBar";
 
 export default function Layout() {
   const [isOpen, setIsOpen] = useState(false);
